f680caef4c26a7c3b4ff50811e6be365b9d0aad8
Ahmed-Egal <egalosman101@gmail.com>
2023-10-29 21:23:34 +0300

    Created the product component and styled it.
    The product component handles the fetching of data from an API and renders it to our page.I have added styling for the products section.

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,9 +1,26 @@
 import React from 'react'
+import './home.css'
+import shoppingStore from '../assets/shopping-store.jpg'
 
 const Home = () => {
   return (
-    <div>
-        <h1>Home</h1>
+    <div className='home'>
+      
+
+
+
+      <div className='paragraph-content'> 
+        <h1>Welcome to Zahir - Where Style Meets Comfort!</h1>
+          <p>Step into a world of fashion and endless possibilities. Discover the latest trends, explore timeless classics, and find the perfect outfit that expresses your unique style.
+
+At Zahir, we're passionate about curating a collection that caters to every taste, occasion, and season. From cozy essentials to show-stopping statement pieces, we have something for everyone.
+
+Join us on this sartorial journey and redefine your wardrobe with us. Happy shopping!"
+
+</p>
+      </div>
+    
+        
       
     </div>
   )
--- src/components/Products.js
+++ src/components/Products.js
@@ -1,10 +1,77 @@
 import React from 'react'
+import {useState,useEffect} from 'react'
+import './products.css'
 
 const Products = () => {
+
+  const[products,setProducts] = useState([])
+  const [cartCount,setCartCount] = useState(0);
+
+  useEffect(() => {
+    async function fetchApi(){
+      try {
+        const response = await fetch('https://fakestoreapi.com/products');
+        const data = await response.json();
+    
+        console.log(data);
+        setProducts(data);
+      } catch(error) {
+        console.log(error);
+      }
+     }
+    
+     fetchApi();
+
+  },[])
+
+  
+
+  const AddToCart = () => {
+    setCartCount(cartCount + 1)
+
+  }
+
+
+
+
   return (
-    <div>
-      <h1>Products</h1>
+    <div className='products-page'>
+      <h1>Welcome to the products section</h1>
+      <p>Browse through our carefully selected range of clothing, meticulously designed to make you look and feel your best. Our commitment is to provide not just clothes, but an experience that resonates with your individuality.</p>
+
+      <div className='cart-card'>
+         <p>
+         You have {cartCount} items in your cart 
+        </p> 
+      </div>
+
+      <div className='products'>
+        {products.map(data => (
+          <div key={data.id}  className='products-card'>
+            <p>{data.title}</p>
+            <img src={data.image} className='product-img'/>
+            <p> {data.price} $ </p>
+            <div>
+            <button onClick={AddToCart}>
+              Add Item
+            </button>
+            </div>
+            
+          </div>
+          
+        ))}
+
+        
+        
+
+      </div>
+
+      
+
     </div>
+
+    
+    
   )
 }
 
